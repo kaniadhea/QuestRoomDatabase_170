8a782e9bf533ffb955402266acb3deb1a49c4867
kaniadhea <kaniadhea7@gmail.com>
2024-12-11 13:32:23 +0700

    snackbarHost = { SnackbarHost(hostState = snackbarHostState)} untuk tampilkan snackbar di scaffold (memperbaiki yang error)

--- app/src/main/java/com/example/roomlocaldb/ui/view/mahasiswa/InserMhsView.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/view/mahasiswa/InserMhsView.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -62,7 +63,7 @@ fun InserMhsView(
 
     Scaffold(
         modifier = modifier,
-        snackbarHost = { (SnackbarHostState = snackbarHostState)}
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState)}
     ) { padding ->
         Column (
             modifier = Modifier
