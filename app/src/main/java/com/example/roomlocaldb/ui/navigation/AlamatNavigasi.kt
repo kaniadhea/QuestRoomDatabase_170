a57c31116c3670ac6e5919d42908cad07d71ea42
kaniadhea <kaniadhea7@gmail.com>
2024-12-15 15:30:10 +0700

    Mempermudah navigasi ke layar detail sambil mengirim parameter seperti nim untuk ditampilkan atau diproses lebih lanjut.

--- app/src/main/java/com/example/roomlocaldb/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/navigation/AlamatNavigasi.kt
@@ -8,6 +8,13 @@ object DestinasiHome : AlamatNavigasi {
     override val route = "home"
 }
 
+object DestinasiDetail : AlamatNavigasi{
+    override  val route = "detail"
+    const val NIM ="nim"
+    val routesWithArg = "$route/ {$NIM}"
+}
+
+
 object  DestinasiUpdate : AlamatNavigasi{
     override val route ="update"
     const val NIM ="nim"
