fbc493869d40cd9908ddc4a8c2fd8eb624c91258
kaniadhea <kaniadhea7@gmail.com>
2024-12-15 14:53:52 +0700

    LocalRepositoryMhs adalah perantara yang mengatur akses data mahasiswa dari database menggunakan DAO

--- app/src/main/java/com/example/roomlocaldb/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,26 @@ package com.example.roomlocaldb.repository
 
 import com.example.roomlocaldb.data.dao.MahasiswaDao
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
-) : RepositoryMhs{
-    override suspend fun insertMhs(mahasiswa: Mahasiswa){
+) : RepositoryMhs {
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+    //getAllMhs
+    override fun getAllMhs() : Flow<List<Mahasiswa>>{
+        return mahasiswaDao.getAllMahasiswa()
+    }
+    override fun getMhs(nim: String) : Flow<Mahasiswa>{
+        return mahasiswaDao.getMhs(nim)
+    }
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+    override suspend fun  updateMhs(mahasiswa:Mahasiswa){
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
 }
+
