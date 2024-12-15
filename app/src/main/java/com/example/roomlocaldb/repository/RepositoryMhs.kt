b763105b1e4d645e4d6a17ba119f426ece78a59f
kaniadhea <kaniadhea7@gmail.com>
2024-12-15 14:10:14 +0700

    metode ini memanfaatkan fungsi get untuk mengambil data, detekemhs untuk memngapus data dan update untuk memperbarui data mahasiswa didatabase

--- app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
@@ -1,8 +1,19 @@
 package com.example.roomlocaldb.repository
 
+import com.example.roomlocaldb.MainActivity
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
 
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
+
 }
