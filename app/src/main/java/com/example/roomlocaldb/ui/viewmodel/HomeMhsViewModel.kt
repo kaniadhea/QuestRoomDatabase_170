a2976e3e9fc9b6681715985968dc8e524eb6ede6
kaniadhea <kaniadhea7@gmail.com>
2024-12-16 16:01:16 +0700

    pada catch {} yaitu untuk menangani error yang mungkin terjadi saat mengambil data dari repository. dan oada stateIn Mengubah flow menjadi StateFlow agar bisa diamati secara reaktif dan memiliki state awal

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
@@ -30,13 +30,32 @@ class HomeMhsViewModel (
             emit(HomeUiState(isLoading = true))
             delay(900)
         }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+
+}
 
 
 
 
 
 
-                    data class HomeUiState (
+
+data class HomeUiState (
     val listMhs: List<Mahasiswa> = listOf(),
     val isLoading : Boolean = false,
     val isError: Boolean = false,
