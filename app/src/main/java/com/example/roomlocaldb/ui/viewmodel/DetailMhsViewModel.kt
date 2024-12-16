745e19d4a0fb058b2595afa4c2a045ef7cb9e561
kaniadhea <kaniadhea7@gmail.com>
2024-12-16 16:33:17 +0700

    membuat data class DetailUiState pada file homemhsviewmodel

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
@@ -4,9 +4,25 @@ import com.example.praktikum8.ui.viewmodel.MahasiswaEvent
 import com.example.roomlocaldb.data.entity.Mahasiswa
 
 
+
 class DetailMhsViewModel(
 
 )
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError : Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty : Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+
 
 fun Mahasiswa.toDetailUiEvent() : MahasiswaEvent {
     return MahasiswaEvent(
