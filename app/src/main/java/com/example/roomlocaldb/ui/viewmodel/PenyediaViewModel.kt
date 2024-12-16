56ff691ca12da297d134c598d38c313637adf258
kaniadhea <kaniadhea7@gmail.com>
2024-12-16 16:46:05 +0700

    Menyediakan dan mengonfigurasi ViewModel secara efisien dengan akses ke repository melalui krsApp()

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.roomlocaldb.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -14,8 +15,20 @@ object PenyediaViewModel {
                 krsApp().containerApp.repositoryMhs
             )
         }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
     }
 }
-
 fun CreationExtras.krsApp(): KrsApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
